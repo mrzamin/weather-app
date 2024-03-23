f96acd271dcd40baf09a0357cd6dbd32c48c436f
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-03-23 16:42:22 -0400

    Add humidity, uv, and wind cards

--- src/view.js
+++ src/view.js
@@ -1,11 +1,78 @@
+//Import dependencies:
+import moment from "moment";
+
 //Grab DOM elements:
 const forecastContainer = document.querySelector(".forecast-days-container");
 const forecastHeader = document.querySelector(".forecast-header");
-//Populate the forecast display container:
-const renderForecast = async (data) => {
-  const weatherData = await data;
-  const weather = weatherData[0];
-  const forecast = weatherData[1];
+const weatherContainer = document.querySelector(".weather-container");
+
+//Populate weather container:
+const renderWeather = (weather) => {
+  clearContainer(weatherContainer);
+
+  const weatherCard = document.createElement("div");
+  weatherCard.classList.add("weather-card");
+
+  const icon = new Image();
+  icon.src = `${weather.icon}`;
+
+  const iconContainer = document.createElement("div");
+  iconContainer.classList.add("icon-container");
+  iconContainer.appendChild(icon);
+
+  const locationHeading = document.createElement("h2");
+  locationHeading.classList.add("location-heading");
+  locationHeading.textContent = `Current weather for ${weather.location}`;
+
+  const description = document.createElement("div");
+  description.textContent = `${weather.description}`;
+
+  const temp = document.createElement("div");
+  temp.textContent = `${weather.temp}`;
+
+  const lastUpdated = document.createElement("div");
+  lastUpdated.textContent = `Last updated ${moment()
+    .startOf("minute")
+    .fromNow()}`;
+
+  const childElements = [
+    iconContainer,
+    locationHeading,
+    description,
+    temp,
+    lastUpdated,
+  ];
+  childElements.forEach((element) => {
+    weatherCard.appendChild(element);
+  });
+
+  weatherContainer.appendChild(weatherCard);
+  //Additional data:
+  const wind = document.createElement("div");
+  wind.textContent = `Wind: ${weather.wind}mph`;
+
+  const humidity = document.createElement("div");
+  humidity.textContent = `Humidity: ${weather.humidity}`;
+
+  const uv = document.createElement("div");
+  uv.textContent = `UV Index: ${weather.uv}`;
+
+  const additionalData = [wind, humidity, uv];
+
+  const weatherStatsContainer = document.createElement("div");
+  weatherStatsContainer.classList.add("weather-stats-container");
+
+  additionalData.forEach((item) => {
+    weatherStatsContainer.appendChild(item);
+  });
+
+  weatherContainer.appendChild(weatherStatsContainer);
+};
+
+//Populate forecast container:
+const renderForecast = (weather, forecast) => {
+  clearContainer(forecastHeader);
+  clearContainer(forecastContainer);
 
   forecastHeader.textContent = `Forecast for ${weather.location}`;
 
@@ -14,25 +81,37 @@ const renderForecast = async (data) => {
     forecastCard.classList.add("forecast-card");
 
     const icon = new Image();
-    icon.src = `${weather.icon}`;
+    icon.src = `${day.icon}`;
 
     const iconContainer = document.createElement("div");
     iconContainer.classList.add("icon-container");
     iconContainer.appendChild(icon);
 
+    const weekday = document.createElement("div");
+    const formattedWeekday = moment(day.date).format("dddd");
+    weekday.textContent = `${formattedWeekday}`;
+
     const date = document.createElement("div");
-    date.textContent = `${weather.date}`;
+    const formattedDate = moment(day.date).format("LL");
+    date.textContent = `${formattedDate}`;
 
     const description = document.createElement("div");
-    description.textContent = `${weather.description}`;
+    description.textContent = `${day.description}`;
 
     const tempHigh = document.createElement("div");
-    tempHigh.textContent = `${forecast.high_f}`;
+    tempHigh.textContent = `H: ${day.high}`;
 
     const tempLow = document.createElement("div");
-    tempLow.textContent = `${forecast.low_f}`;
+    tempLow.textContent = `L: ${day.low}`;
 
-    const childElements = [iconContainer, date, description, tempHigh, tempLow];
+    const childElements = [
+      iconContainer,
+      weekday,
+      date,
+      description,
+      tempHigh,
+      tempLow,
+    ];
 
     childElements.forEach((element) => {
       forecastCard.appendChild(element);
@@ -41,4 +120,18 @@ const renderForecast = async (data) => {
   });
 };
 
-export { renderForecast };
+//Clear a container:
+const clearContainer = (container) => {
+  container.innerHTML = "";
+};
+
+//Render weather and forecast together:
+const renderWeatherAndForecast = async (data) => {
+  const weatherData = await data;
+  const weather = weatherData[0]; //object representing current weather
+  const forecast = weatherData[1]; //array of objects representing forecast days
+  renderWeather(weather);
+  renderForecast(weather, forecast);
+};
+
+export { renderWeatherAndForecast };
